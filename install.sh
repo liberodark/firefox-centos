546e64e641537b27e8202b094cf90842cac4ce16
liberodark <liberodark@gmail.com>
2019-08-26 13:41:42 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# About: Install Firefox automatically
+# Author: liberodark
+# License: GNU GPLv3
+
+version="0.0.1"
+
+echo "Welcome on Firefox Install Script $version"
+
+#=================================================
+# CHECK ROOT
+#=================================================
+
+if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
+
+install_firefox(){
+      
+      pushd /usr/local/ || exit
+      tar xvjf firefox-*.tar.bz2 &> /dev/null
+      sudo rm firefox-*.tar.bz2
+      ln -s /usr/local/firefox/firefox /usr/bin/firefox
+      popd || exit
+      }
+
+
+if ! command -v firefox &> /dev/null; then
+
+    if [[ "$distribution" = CentOS || "$distribution" = CentOS || "$distribution" = Red\ Hat || "$distribution" = Fedora || "$distribution" = Suse || "$distribution" = Oracle ]]; then
+      yum remove -y firefox* &> /dev/null
+
+      install_firefox || exit
