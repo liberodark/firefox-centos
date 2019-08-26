08217dd839d0ce6cad26d9e8016fdc66fa2393db
liberodark <liberodark@gmail.com>
2019-08-26 17:07:01 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ read -r lang
 rhel_firefox(){
       
       pushd /usr/local/ || exit
-      wget https://download-installer.cdn.mozilla.net/pub/firefox/releases/${firefox_version}/linux-x86_64/${lang}/firefox-68.0.2.tar.bz2
+      wget https://download-installer.cdn.mozilla.net/pub/firefox/releases/${firefox_version}/linux-x86_64/${lang}/firefox-${firefox_version}.tar.bz2
       tar xvjf firefox-*.tar.bz2 &> /dev/null
       sudo rm firefox-*.tar.bz2
       ln -s /usr/local/firefox/firefox /usr/bin/firefox
